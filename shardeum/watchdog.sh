e966883d7df411a0d46b812c2f84ff52a4ce5f5a
bombermine <bombermine3@gmail.com>
2023-03-17 12:13:06 +0300

    Refactorig

--- shardeum/watchdog.sh
+++ shardeum/watchdog.sh
@@ -2,21 +2,22 @@
 
 lock_file='watchdog.lock'
 
-lock() { [ -f $lock_file ] && exit || touch $lock_file ; }
-unlock() { rm $lock_file ; }
-get_status() { docker exec -t shardeum-dashboard operator-cli status | sed -ne 's/state: \(.*\)/\1/p' | tr -d "\n\r" ; }
+get_status() { docker exec -t shardeum-dashboard operator-cli status | sed -ne 's/state: \(.*\)/\1/p' | tr -d '\n\r' ; }
 start_shardeum() { docker exec -t shardeum-dashboard operator-cli start ; }
 log_time() { date '+%Y-%m-%d %H:%M:%S' ; }
 
 cd "${0%/*}"
 
-lock
+if { set -C; 2>/dev/null >$lock_file; }; then
+    trap "rm -f $lock_file" EXIT
+else
+    exit
+fi
 
 while [ $(get_status) = 'stopped' ]; do
-        echo $(log_time) 'Shardeum is stopped. Trying to start'
-        start_shardeum
-        sleep 15
+    echo $(log_time) 'Shardeum is stopped. Trying to start'
+    start_shardeum
+    sleep 15
 done
 
 echo $(log_time) 'Shardeum is started'
-unlock
